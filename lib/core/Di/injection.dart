b4f9d312e4c5d78a36be84d6b3507544600dc794
nimaaqakhani <nimaaqakhani2@gmail.com>
2025-03-16 00:22:06 +0330

    refactor_Injection

--- lib/core/Di/injection.dart
+++ lib/core/Di/injection.dart
@@ -2,15 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:flutter_application_1/core/services/api_service.dart';
 import 'package:flutter_application_1/features/GlobalCountries/data/datasources/country_remote_data_source.dart';
 import 'package:flutter_application_1/features/GlobalCountries/data/repositories/country_repository_impl.dart';
+import 'package:flutter_application_1/features/GlobalCountries/domain/repositories/country_repository.dart';
+import 'package:flutter_application_1/features/GlobalCountries/domain/usecases/get_countries.dart';
+import 'package:flutter_application_1/features/GlobalCountries/presentation/bloc/country_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
+
 class Injection {
   static void init() {
     getIt.registerLazySingleton<Dio>(() => Dio());
     getIt.registerLazySingleton<ApiService>(() => ApiService());
-    getIt.registerLazySingleton<CountryApi>(() => CountryApi(getIt<ApiService>()));
-    getIt.registerLazySingleton<CountryRepositoryImpl>(
+    getIt.registerLazySingleton<CountryApi>(
+        () => CountryApi(getIt<ApiService>()));
+    getIt.registerLazySingleton<CountryRepository>(
         () => CountryRepositoryImpl(getIt<CountryApi>()));
+    getIt.registerLazySingleton(() => GetCountries(getIt<CountryRepository>()));
   }
 }
